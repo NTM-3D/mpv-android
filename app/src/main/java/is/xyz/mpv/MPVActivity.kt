16e7ee661be6c7f720507df760285d7a2261c325
NTM-3D <268098387+NTM-3D@users.noreply.github.com>
2026-07-14 08:51:17 +0200

    PGS subtitle debugging.

--- app/src/main/java/is/xyz/mpv/MPVActivity.kt
+++ app/src/main/java/is/xyz/mpv/MPVActivity.kt
@@ -2969,7 +2969,7 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
                 Enable3D()
             }
         }
-        applyLeiaDisplayProperties(format, leiaEnabled)
+        //applyLeiaDisplayProperties(format, leiaEnabled)
         updateLeiaContentAspect()
         updateOrientation()
         mPrevDesiredBacklightModeState = leiaEnabled
