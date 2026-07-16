26884c494d9d498edaa90f7ea1ff5b1a9b9e90c4
NTM-3D <268098387+NTM-3D@users.noreply.github.com>
2026-07-16 08:28:51 +0200

    Fix setting default value setting for 3D PGS subtitles.

--- app/src/main/java/is/xyz/mpv/MPVActivity.kt
+++ app/src/main/java/is/xyz/mpv/MPVActivity.kt
@@ -3170,6 +3170,7 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
         imageSubtitle3DCheck.setOnCheckedChangeListener { _, isChecked ->
             imageSubtitle3D = isChecked
             if (isChecked) imageSubsScaleXSeekBar.progress = 9
+            if (isChecked) imageSubsScaleYSeekBar.progress = 9
             applyImageSubtitleStereoMode()
             applyImageSubtitlePosition(imageSubtitlePosition)
             applyImageSubtitleScale(imageSubtitleScale)
